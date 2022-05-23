47734459db8b9eb9198129dadd4e699130dc5d86
elaristocarata <victorhugomendozajimenez@gmail.com>
2022-05-22 21:27:24 -0500

    Crate first page

---
+++ pages/aboutus.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+function AboutUs(props) {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <Link href="/">Home</Link>
+        </div>
+    );
+}
+
+export default AboutUs;

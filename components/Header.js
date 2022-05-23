c5e3dd163ff7d1473ff6a1ddd76d734381f1ad48
elaristocarata <victorhugomendozajimenez@gmail.com>
2022-05-22 22:10:49 -0500

    Adding header styles

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,12 @@
 import Link from "next/link";
-
+import styles from '../styles/Header.module.css'
 
 function Header() {
     return (
-        <header>
+        <header className={styles.header}>
             <div className="contenedor">
                 <div>
-                    <nav>
+                    <nav className={styles.nave}>
                         <Link href="/">Home</Link>
                         <Link href="/store">Store</Link>
                         <Link href="/blog">Blog</Link>
